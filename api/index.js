5b45f605a99576d8eb5691fdc5599118b12bc558
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-10-11 19:12:32 +0300

    Fix build issues

--- api/index.js
+++ api/index.js
@@ -1,29 +1,21 @@
-// api/index.js
-require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// Initialize express app
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // Use built-in json parser
 
-// Connect to MongoDB
-const connectToDatabase = async () => {
-    if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('MongoDB connected');
-    }
-};
+// Connect to MongoDB using the environment variable
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log('MongoDB connection error:', err));
 
 // Import routes
-const StudentsRoutes = require('./router/StudentsRouter'); 
-const MembersRouter = require('./router/MembersRouter'); 
+const StudentsRoutes = require('./router/StudentsRouter');
+const MembersRouter = require('./router/MembersRouter');
 const ClubsRouter = require('./router/ClubsRouter');
 const SportsRouter = require('./router/SportsRouter');
 const CulturalRouter = require('./router/CulturalRouter');
@@ -31,12 +23,6 @@ const SocialRouter = require('./router/SocialRouter');
 const ScoutRouter = require('./router/ScoutRouter');
 const ScientificRouter = require('./router/ScientificRouter');
 const ArtsRouter = require('./router/ArtsRouter');
-
-// Connect to database before defining routes
-app.use(async (req, res, next) => {
-    await connectToDatabase();
-    next();
-});
 
 // Routes
 app.use('/students', StudentsRoutes);
@@ -49,11 +35,7 @@ app.use('/scout', ScoutRouter);
 app.use('/scientific', ScientificRouter);
 app.use('/arts', ArtsRouter);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-// Export the app for serverless deployment
-module.exports = app;
+// Export the app as a serverless function
+module.exports = (req, res) => {
+    app(req, res);
+};
